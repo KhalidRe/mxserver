56ed5625139738d1dc717968492e54b154058400
Khaled Tumah <khaledtumah@gmail.com>
2022-01-06 17:09:35 +0100

    init

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+const cors = require("cors");
 var path = require("path");
 var fs = require("fs");
 var PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 var db = mysql.createConnection({
     multipleStatements: true,
     user: "doadmin",
