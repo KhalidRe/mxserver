1910460b8d81e7000ccaa2146d47362b22cd077b
Khaled Tumah <khaledtumah@gmail.com>
2022-01-06 17:46:34 +0100

    init

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ db.connect((err) => {
     if (err) throw err;
     console.log("Mysql Connected");
 });
-app.use(
-    cors({
-        origin: "*",
-    })
-);
+app.use(cors());
 app.use(
     session({
         secret: "secret",
@@ -46,7 +42,10 @@ app.use(express.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
 
