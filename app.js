93ee99a48c7f75083642408ce44e6fe00935f936
Khaled Tumah <khaledtumah@gmail.com>
2022-01-18 16:44:50 +0100

    correction

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin: http://localhost:3000");
+    res.header("Access-Control-Allow-Origin: http://192.168.1.65:8080");
     res.header("Access-Control-Allow-Credentials: true");
     res.header("Access-Control-Allow-Methods", "GET,POST");
     next();
