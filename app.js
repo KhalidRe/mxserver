d939e792224d1882673232c7746e2cd5f1f5be97
KhalidRe <khaledtumah@gmail.com>
2022-01-25 11:30:30 +0100

    socketemit

--- app.js
+++ app.js
@@ -97,12 +97,13 @@ app.post("/loggedin", (req, res) => {
     res.json(result);
   });
 });
+
 app.get("/viewprojects", (req, res) => {
   let sql = "SELECT * FROM projects";
   let query = db.query(sql, (err, result) => {
     if (err) throw err;
 
-    res.json(result);
+    io.sockets.emit("viewprojects", res.json(result));
   });
 });
 app.post("/myprojects", (req, res) => {
