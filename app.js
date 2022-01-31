432645bafaba2f7a54e55cc9c657204f2267e20f
KhalidRe <khaledtumah@gmail.com>
2022-01-31 08:51:47 +0100

    alltimeget

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post("/loggedin", (req, res) => {
   let maker = {
     user: req.body.user,
   };
-  let sql = `SELECT Username FROM users WHERE Username = '${maker.user}'`;
+  let sql = `SELECT Username,Status FROM users WHERE Username = '${maker.user}'`;
   let query = db.query(sql, (err, result) => {
     if (err) throw err;
     res.json(result);
@@ -130,6 +130,14 @@ app.post("/mytime", (req, res) => {
     user: req.body.user,
   };
   let sql = `SELECT * FROM time  WHERE Username = '${maker.user}'`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.json(result);
+  });
+});
+app.post("/alltime", (req, res) => {
+  let sql = `SELECT * FROM time `;
   let query = db.query(sql, (err, result) => {
     if (err) throw err;
     console.log(result);
